cc410909366d27629c79689deaa4a9dd7c3c6c25
Aleksey Yesin <esinru@gmail.com>
2021-04-06 07:30:29 +0300

    isContactExists

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import { Component } from 'react';
 import { v4 as uuid } from 'uuid';
 // import css from './styles/App.module.scss';
+import config from './json/AppConfig.json';
 import ContactForm from './components/ContactForm';
 import ContactList from './components/ContactList';
 import Filter from './components/Filter';
 
 class App extends Component {
     state = {
-        contacts: [],
+        contacts: config.INIT_CONTACTS,
         filter: '',
     };
 
     addContact = ({ name, number }) => {
+        if (this.isContactExists(name)) {
+            alert(`Контакт "${name}" уже существует`);
+            return;
+        }
+
         const newContact = { id: uuid(), name, number };
 
         this.setState(({ contacts }) => ({
@@ -22,6 +28,14 @@ class App extends Component {
     changeFilter = e => {
         this.setState({ filter: e.target.value });
     };
+
+    isContactExists(name) {
+        const normName = name.toLowerCase();
+
+        return !!this.state.contacts.find(
+            ({ name }) => name.toLowerCase() === normName,
+        );
+    }
 
     getFilteredContacts() {
         const normFilter = this.state.filter.toLowerCase();
